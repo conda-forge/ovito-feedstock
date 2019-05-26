a215a5daf7bbe2b6ce17ff57cf7cbf18e41c5b9d
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-05-26 13:15:33 +0200

    lib64

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,11 +7,12 @@ ls /usr/include/
 cp -r /usr/include/libssh ${PREFIX}/include
 echo 'ls /usr/lib64/libssh*'
 ls /usr/lib64/libssh*
-cp -r /usr/lib64/libssh* ${PREFIX}/lib
+cp -r /usr/lib64/libssh* ${PREFIX}/lib64
 
 args="-DOVITO_BUILD_DOCUMENTATION=OFF \
       -DCMAKE_BUILD_TYPE=Release \
-      -DLIBSSH_INCLUDE_DIR=/usr/include \
+      -DLIBSSH_LIBRARY=${PREFIX}/lib64 \
+      -DLIBSSH_INCLUDE_DIR=${PREFIX}/include \
       -DCMAKE_INSTALL_PREFIX=${PREFIX}" 
 
 mkdir build
@@ -37,4 +38,4 @@ cp lib/ovito/plugins/*.so ${PREFIX}/lib/ovito/plugins
 sed -i 's\/../../..\/../../../../ovito/plugins\g' ${SP_DIR}/ovito/plugins/__init__.py
 
 rm -rf ${PREFIX}/include/libssh
-rm -rf ${PREFIX}/lib/libssh* 
+rm -rf ${PREFIX}/lib64/libssh* 
