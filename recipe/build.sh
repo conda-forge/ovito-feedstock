8da11a3f6b90e2cc98aede50998b0f2c82c42c4b
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-05-27 16:50:50 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,12 @@ args="-DOVITO_BUILD_DOCUMENTATION=OFF \
       -DLIBSSH_LIBRARY=${PREFIX}/lib/libssh.so \
       -DLIBSSH_INCLUDE_DIR=${PREFIX}/include/libssh \
       -DOVITO_USE_PRECOMPILED_HEADERS=OFF \
+      -DOVITO_RELATIVE_BINARY_DIRECTORY=bin \
+      -DOVITO_RELATIVE_LIBRARY_DIRECTORY=lib/ovito \
+      -DOVITO_RELATIVE_3RDPARTY_LIBRARY_DIRECTORY=lib/ovito \
+      -DOVITO_RELATIVE_SHARE_DIRECTORY=share/ovito \
+      -DOVITO_RELATIVE_PLUGINS_DIRECTORY=lib/ovito \
+      -DOVITO_RELATIVE_PYTHON_DIRECTORY=lib/ovito/plugins/python \
       -DCMAKE_INSTALL_PREFIX=${PREFIX}" 
 
 mkdir build
@@ -25,8 +31,8 @@ fi;
 
 make 
 
-mkdir -p ${PREFIX}/lib/ovito/plugins
-cp lib/ovito/*${SHLIB_EXT} ${PREFIX}/lib/ovito/plugins
+mkdir -p ${PREFIX}/lib/ovito
+cp lib/ovito/*${SHLIB_EXT} ${PREFIX}/lib/ovito
 cp -r lib/ovito/plugins/python/ovito ${SP_DIR}
-cp lib/ovito/plugins/*${SHLIB_EXT} ${PREFIX}/lib/ovito/plugins
+cp lib/ovito/plugins/*${SHLIB_EXT} ${PREFIX}/lib/ovito
 sed -i 's\/../../..\/../../../../ovito/plugins\g' ${SP_DIR}/ovito/plugins/__init__.py
